3fe382d5273b8acbd41ce609d08dc0643adb3a4f
olfaMK <114759432+olfa8687@users.noreply.github.com>
2022-10-15 15:41:14 +0300

    Create Block.js

---
+++ Block.js
@@ -0,0 +1,32 @@
+const SHA256 = require('crypto-js/sha256');
+const hex2ascii = require('hex2ascii');
+
+class Block {
+    // Constructor - argument data will be the object containing the transaction data
+	constructor(data){
+		this.hash = null;                                           // Hash of the block
+		this.height = 0;                                            // Block Height (consecutive number of each block)
+		this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+		this.time = 0;                                              // Timestamp for the Block creation
+		this.previousBlockHash = null;                              // Reference to the previous Block Hash
+    } 
+    validate() {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            const currentBlockHash=self.hash ; 
+            self.hash=null;  
+             // Recalculate the hash of the Block
+             const R_hash=SHA256(JSON.stringify(currentBlockHash)).toString;
+           
+             console.log(`self Hash: ${self.hash}`);// to diplay the self hash
+             console.log(`R Hash: ${R_hash}`);//to display the recalculated hash of the block              
+             if (R_hash!== self.hash){reject(false);}
+            else{
+                // Returning the Block is valid
+                resolve(true);}
+            });
+   }
+
+}
+
+module.exports.Block = Block;                    // Exposing the Block class as a module
